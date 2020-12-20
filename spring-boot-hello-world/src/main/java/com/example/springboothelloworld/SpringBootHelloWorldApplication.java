31e257c5188825877f21b5cad81d0e6c25e28b82
YS.YOON <bozzang@gmail.com>
2020-12-20 20:59:28 +0900

    bean provider

--- spring-boot-hello-world/src/main/java/com/example/springboothelloworld/SpringBootHelloWorldApplication.java
+++ spring-boot-hello-world/src/main/java/com/example/springboothelloworld/SpringBootHelloWorldApplication.java
@@ -3,7 +3,6 @@ package com.example.springboothelloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootHelloWorldApplication {
@@ -20,11 +19,6 @@ public class SpringBootHelloWorldApplication {
 
 	public void hello() {
 		System.out.println("Hello Spring Boot!!");
-	}
-	
-	@Bean
-	public People getNmae() {
-		return new People("y2sang");
 	}
 
 }
