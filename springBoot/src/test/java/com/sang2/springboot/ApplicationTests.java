dc0e40775d7cde5f019e4f34217894b61767b8d0
YS.YOON <bozzang@gmail.com>
2021-08-08 20:38:48 +0900

    .

---
+++ springBoot/src/test/java/com/sang2/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sang2.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
